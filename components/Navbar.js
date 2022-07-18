0d175aae0f3deb7604d038bd37f5e91e87d99d19
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-07-18 15:52:42 -0700

    feat: update navbar blur

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,9 +33,9 @@ const Navbar = () => {
   // }, []);
 
   const bg = navbar ? "backdrop-blur-xl" : "bg-transparent";
-  const side = sidebar ? "fixed" : "absolute";
+  const side = sidebar ? "fixed" : "";
   const always =
-    "w-full h-min-screen flex flex-row justify-between items-center px-16 py-8 lg:px-32 z-50 transition-all overflow-hidden";
+    "w-full h-min-screen fixed flex backdrop-blur-md bg-black/20 flex-row justify-between items-center px-16 py-8 lg:px-32 z-50 transition-all overflow-hidden";
 
   const animate1 = sidebar ? "rotate-45 translate-y-1.5" : "";
   const animate2 = sidebar ? "-rotate-45 -translate-y-1" : "";
